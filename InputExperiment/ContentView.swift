b473c6d6a63824fed2bd381e505a9a0fe6ac5b9d
Sam Garson <samtgarson@gmail.com>
2020-10-04 11:36:27 +0100

    Remove UI tests and add a button

--- InputExperiment/ContentView.swift
+++ InputExperiment/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Send message") { sendMessage() }
+    }
+    
+    private func sendMessage() {
+        let uuid = UUID().uuidString
+        print(uuid)
     }
 }
 
