3292e5ae88b1bfcef7de35a065022c032cc6df03
Sam Garson <samtgarson@gmail.com>
2020-10-04 10:35:05 +0100

    Initial Commit

---
+++ InputExperiment/InputExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputExperimentApp.swift
+//  InputExperiment
+//
+//  Created by Sam Garson on 04/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct InputExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
