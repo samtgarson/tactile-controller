a849e8a89288a6e60273168e856775625842316c
Sam Garson <samtgarson@gmail.com>
2021-01-01 14:43:34 +0000

    App Icon and better assets

--- web/components/nav/nav-bar.tsx
+++ web/components/nav/nav-bar.tsx
@@ -3,13 +3,14 @@ import Link from 'next/link'
 import { Navbar } from 'rbx'
 import { Sun, Moon, GitHub } from 'react-feather'
 import { useTheme } from 'next-themes'
+import Image from 'next/image'
 import styles from './nav.module.scss'
 
 export const NavBar = () => {
   const { theme, setTheme } = useTheme()
   const ThemeSwitcher = useMemo(() => theme === 'dark' ? Moon : Sun, [theme])
   const navColor = useMemo(() => theme === 'dark' ? 'dark' : 'white', [theme])
-  const icon = useMemo(() => theme === 'dark' ? '/hand-light.png' : 'hand.png', [theme])
+  const icon = useMemo(() => theme === 'dark' ? '/hand-light.png' : '/hand.png', [theme])
   const switchTheme = useCallback(() => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }, [setTheme, theme])
@@ -18,7 +19,13 @@ export const NavBar = () => {
     <Navbar.Brand>
       <Link passHref href="/">
         <Navbar.Item  className={styles.title}>
-          <img src={icon}/>
+          <Image
+            src={icon}
+            alt="Tactile Controller Icon (a finger swiping)"
+            width={23}
+            height={20}
+            layout="fixed"
+          />
           Tactile Controller
         </Navbar.Item>
       </Link>
@@ -26,8 +33,8 @@ export const NavBar = () => {
     </Navbar.Brand>
     <Navbar.Menu className={styles.divider}>
       <Navbar.Segment align="end">
-        <Navbar.Item>
-          <ThemeSwitcher size={18} onClick={switchTheme} />
+        <Navbar.Item onClick={switchTheme}>
+          <ThemeSwitcher size={18} />
         </Navbar.Item>
         <Link passHref href="https://github.com/samtgarson/tactile-controller">
           <Navbar.Item><GitHub size={18} /></Navbar.Item>
